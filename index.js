b5c2d5066a69cd4f28cabf359b250e4444fbc5c6
Mansi Deshmukh <102024693+Mansi-Deshmukh@users.noreply.github.com>
2022-11-15 15:10:24 +0000

    Update index.js

--- index.js
+++ index.js
@@ -33,3 +33,10 @@ sr.reveal('.about,.services,.portfolio,.contact',{delay:200, origin:'bottom'})
 // 	const span = document.querySelector("")
 // })
 
+let resumeSec = document.querySelectorAll("#download")
+resumeSec.forEach((el)=> el.addEventListener("click",()=>{
+    window.open(
+        'https://drive.google.com/file/d/1gRGPY--iLNKflls24xzLRTAQ8Yfm7aIA/view?usp=share_link',
+        '_blank'
+      );
+})) 
